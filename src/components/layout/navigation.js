3e0bfdb024558c65a50c01415a556b86232e88f2
Aman Kumar <amantheroot@gmail.com>
2019-04-28 11:38:01 +0530

    added media

--- src/components/layout/navigation.js
+++ src/components/layout/navigation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import {Link} from "react-router-dom";
+
+import Logo from "../../assets/media/logo.svg";
+
 export class Navigation extends Component {
   render() {
     return (
       <nav>
-        <h1>Nav</h1>
+        <Link to="/"><img src={Logo} alt="logo"></img></Link>
+        <ul>
+          <li><Link to ="/">Home</Link></li>
+          <li><Link to ="/products">Products</Link></li>
+          <li><Link to ="/contact">Contact</Link></li>
+        </ul>
       </nav>
     )
   }
